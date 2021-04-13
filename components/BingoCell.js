61b4705441af563f3227b493fd9b5181742de999
Justin Thareja <justin.thareja@gmail.com>
2021-04-13 15:14:47 -0400

    adds marker images

--- components/BingoCell.js
+++ components/BingoCell.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import classNames from "classnames/bind";
 import styles from "./BingoGame.module.css";
 
@@ -8,7 +9,17 @@ export function BingoCell({ value, onClick, isChecked, isWinner }) {
     // eslint-disable-next-line
     <td className={styles.cell} onClick={onClick}>
       {value}
-      {isChecked && <div className={cx({ marker: true, isWinner })}></div>}
+      {isChecked && (
+        <div className={cx({ marker: true, isWinner })}>
+          <Image
+            priority
+            width="100%"
+            height="100%"
+            src={`/images/watercolor-circle-${isWinner ? "red" : "blue"}.png`}
+            alt="Blue circle"
+          />
+        </div>
+      )}
     </td>
   );
 }

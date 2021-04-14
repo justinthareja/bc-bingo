adee1b5768e1c9790d242bd1e03af3beb2ed70bb
Justin Thareja <justin.thareja@gmail.com>
2021-04-14 13:00:34 -0400

    removes duplicate instantition of socket client

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,5 @@
-import App from "next/app";
-import io from "socket.io-client";
 import "../styles/global.css";
 
-class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-  state = {
-    socket: null,
-  };
-  componentDidMount() {
-    // connect to WS server and listen event
-    const socket = io();
-    this.setState({ socket });
-  }
-
-  // close socket connection
-  componentWillUnmount() {
-    this.state.socket.close();
-  }
-
-  render() {
-    const { Component, pageProps } = this.props;
-    return <Component {...pageProps} socket={this.state.socket} />;
-  }
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp;

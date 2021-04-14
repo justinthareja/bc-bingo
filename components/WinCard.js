e45a4256e4aced5f463bc55f7d4e6c4b64013c71
Justin Thareja <justin.thareja@gmail.com>
2021-04-14 15:08:58 -0400

    adds new game button

--- components/WinCard.js
+++ components/WinCard.js
@@ -1,29 +1,40 @@
+import { useRouter } from "next/router";
+
 export function WinCard({ winner, socket, congrats }) {
+  const router = useRouter();
   const emitCongrats = (message) => () => {
     socket.emit("congrats", { message, to: winner.userID });
   };
 
-  if (winner === "You") {
-    return (
-      <div>
-        <h1>You Win!</h1>
-        <ul>
-          {congrats.map(({ from, message }, i) => (
-            <li key={`${from}-${i}`}>
-              {message} - {from}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-
   return (
     <div>
-      <h1>{winner.username} Wins!</h1>
-      <h2>Send Congrats</h2>
-      <button onClick={emitCongrats("Good Job")}>Good Job</button>
-      <button onClick={emitCongrats("Nice Work!")}>Congrats!</button>
+      <button
+        onClick={() => {
+          socket.disconnect();
+          router.push("/");
+        }}
+      >
+        New Game
+      </button>
+      {winner === "You" ? (
+        <div>
+          <h1>You Win!</h1>
+          <ul>
+            {congrats.map(({ from, message }, i) => (
+              <li key={`${from}-${i}`}>
+                {message} - {from}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div>
+          <h1>{winner.username} Wins!</h1>
+          <h2>Send Congrats</h2>
+          <button onClick={emitCongrats("Good Job")}>Good Job</button>
+          <button onClick={emitCongrats("Nice Work!")}>Congrats!</button>
+        </div>
+      )}
     </div>
   );
 }
